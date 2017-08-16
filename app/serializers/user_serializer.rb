73a869ac6dc6eaef9b9345d94c64b37aa68ee1b3
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 17:34:16 -0400

    binding

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :callsign, :firstname, :lastname, :email, :password_digest
+  def password_digest
+    binding.pry
+  end
+
   # attributes :id, :callsign, :firstname, :lastname, :email, :level, :rank, :admin, :sash_id, :password_digest
 
 end
