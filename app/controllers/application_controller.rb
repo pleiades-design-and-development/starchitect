ec76b0020578e6f4b91762912c13ef8c18a5f7ca
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 15:46:58 -0400

    installing rack-cors v2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-  after_action :cors_headers
-
-def cors_headers
-  headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-  headers['Access-Control-Request-Method'] = '*'
-  headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-end
+#   after_action :cors_headers
+#
+# def cors_headers
+#   headers['Access-Control-Allow-Origin'] = '*'
+#   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+#   headers['Access-Control-Request-Method'] = '*'
+#   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+# end
 
   include ActionController::Serialization
 end
