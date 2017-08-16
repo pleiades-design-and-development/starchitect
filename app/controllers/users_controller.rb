57914b1ac5a6cafa419f50db867a3b3da100c423
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 13:42:26 -0400

    fixed API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   def index
-    @users = Users.all
+    @users = User.all
     render json: @users
   end
 
