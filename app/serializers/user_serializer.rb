7de82aa1413218539529eb9ca53fb632882b1745
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 17:41:35 -0400

    fixed user controller

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :callsign, :firstname, :lastname, :email, :password_digest
-  def password_digest
-    binding.pry
-  end
+  # def password_digest
+  #   binding.pry
+  # end
 
   # attributes :id, :callsign, :firstname, :lastname, :email, :level, :rank, :admin, :sash_id, :password_digest
 
