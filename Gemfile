ce6740a485c0ef5e685b9af68cd0bcae2d214a5d
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 14:10:51 -0400

    seeded db

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'wicked'
 gem 'merit'
 gem 'redcarpet'
 gem 'active_model_serializers'
+gem 'pry'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  User.create([{firstname: Faker::StarWars.specie}, lastname: Faker::StarWars.vehicle, callsign: Faker::StarWars.planet])
+end
