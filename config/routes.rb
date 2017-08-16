6f6965b7f65ef806baac24201f03fda4adffed3c
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 13:29:25 -0400

    API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
 
-  resources :submissions
-  resources :posts
+scope '/api' do
+  scope '/v1' do
+    scope '/users' do
+      get '/' => 'users#index'
+      post '/' => 'users#create'
+    end
+  end
+end
 
-  resources :users
-  
+
+
+
+  # resources :submissions
+  # resources :posts
+  #
+  # resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
