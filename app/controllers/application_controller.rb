7d1027d1de9890b908fe61c9d87a2ba1aae1b637
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 16:00:04 -0400

    cors preflight check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,24 @@
 class ApplicationController < ActionController::API
-#   after_action :cors_headers
-#
-# def cors_headers
-#   headers['Access-Control-Allow-Origin'] = '*'
-#   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-#   headers['Access-Control-Request-Method'] = '*'
-#   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-# end
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+
+    end
+  end
   include ActionController::Serialization
 end
