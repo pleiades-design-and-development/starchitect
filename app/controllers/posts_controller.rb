0d079a8a6fa5ca2f01d00a2b153e397214eff5ff
Joshua Flack <joshu.alexander@gmail.com>
2017-08-15 21:44:00 -0400

    posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,45 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  def index
+    @posts = posts.all
+    render json: @posts
+  end
+
+  def show
+    render json: @post
+  end
+
+
+  def create
+    @post = post.new(post_params)
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_post
+    @post = post.find(params[:id])
+  end
+
 end
