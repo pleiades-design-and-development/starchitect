a1714115f14ffe1f7703262c3da8240e68564eb5
Joshua Flack <joshu.alexander@gmail.com>
2017-08-16 16:05:48 -0400

    user require remove

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:firstname, :lastname, :email, :callsign, :rank, :level, :password, :password_confirmation)
+    params.permit(:firstname, :lastname, :email, :callsign, :rank, :level, :password, :password_confirmation)
   end
 
   def set_user
