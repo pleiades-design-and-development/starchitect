672eae897b4f36e39ba5f0d7270fbe0c44cfe10c
Joshua Flack <joshu.alexander@gmail.com>
2017-08-17 14:27:32 -0400

    token is up

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
+  
   attributes :callsign, :firstname, :lastname, :email
   has_many :badges
-  # def password_digest
-  #   binding.pry
-  # end
-
-  # attributes :id, :callsign, :firstname, :lastname, :email, :level, :rank, :admin, :sash_id, :password_digest
 
 end
