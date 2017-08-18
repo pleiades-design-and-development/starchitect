5d16c53c936921e255e63dd646361bf6270def6b
Joshua Flack <joshu.alexander@gmail.com>
2017-08-18 15:51:32 -0400

    fixed I think again

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
 
-  attributes :callsign, :firstname, :lastname, :email, :rank, :level
+  attributes :callsign, :firstname, :lastname, :email, :rank, :level, :api_token
   has_many :badges
 
 end
