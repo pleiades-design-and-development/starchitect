b08be92c7eb3d60cb1fc63abd62ae45e00ac9f1a
Joshua Flack <joshu.alexander@gmail.com>
2017-08-18 13:20:06 -0400

    serializer is dumb

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  
-  attributes :callsign, :firstname, :lastname, :email
+
+  attributes :callsign, :firstname, :lastname, :email, :password, :password_confirmation
   has_many :badges
 
 end
