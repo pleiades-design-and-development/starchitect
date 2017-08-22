4c6775b6c274a254ea5a9fd74cddb8b53a226a7b
Joshua Flack <joshu.alexander@gmail.com>
2017-08-22 17:04:23 -0400

    strong params

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -46,6 +46,6 @@ class SubmissionsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def submission_params
-      params.require(:submission).permit(:type, :object, :title, :body, :user_id)
+      params.permit(:type, :object, :title, :body, :user_id)
     end
 end
