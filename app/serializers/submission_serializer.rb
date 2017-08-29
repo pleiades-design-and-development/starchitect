aee574e1a850822acfe45909b03bb8a6ba4987af
Joshua Flack <joshu.alexander@gmail.com>
2017-08-29 15:26:16 -0400

    submission serializer v2

--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -1,9 +1,9 @@
 class SubmissionSerializer < ActiveModel::Serializer
-  attributes :id, :submit_type, :title, :body, :submit_object
+  attributes :id, :submit_type, :title, :body, :submit_object, :callsign
   has_one :user
 
   def callsign
     object.user.callsign
   end
-   
+
 end
